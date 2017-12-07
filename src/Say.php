86bf84101b31bc45b9e434a127de99d967b6c11a
zckongbai <zckongbai@163.com>
2017-12-07 17:43:08 +0800

    ok

---
+++ src/Say.php
@@ -0,0 +1,10 @@
+<?php
+namespace Hello;
+
+Class Say 
+{
+	public static function hello()
+	{
+		return "hello world";
+	}
+}
---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . "/vendor/autoload.php";
+
+Use Hello\Say;
+
+echo Say::hello();
